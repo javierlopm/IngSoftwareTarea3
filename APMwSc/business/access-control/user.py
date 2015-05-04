b6523692b20d62a40393a6f0bd2eca59551803f1
Javier Lopez <javierlopm@hotmail.com>
2015-05-04 10:08:13 -0430

    Modelo de usuario creado

--- APMwSc/business/access-control/user.py
+++ APMwSc/business/access-control/user.py
@@ -1,1 +1,10 @@
- 
+from sqlalchemy.sql.schema import PrimaryKeyConstraint
+class clsUser(db.Model):
+    __tablename__ = 'user'
+    fullname = db.column(db.string(50),              unique=False)
+    username = db.column(db.string(16),         primary_key=True )
+    password = db.column(db.string(16),              unique=False)
+    email    = db.column(db.string(30),              unique=True )
+    iddpt    = db.column(db.Integer   ,db.ForeignKey('dpt.iddpt'))
+    idrole   = db.column(db.Integer   ,db.ForeignKey('role.idrole'))
+    
--- APMwSc/data/model.py
+++ APMwSc/data/model.py
@@ -1,1 +1,18 @@
- 
+class User:
+     def __init__(self,fullname,username,password,email,iddpt,idrole):
+            self.fullname = fullname
+            self.username = username
+            self.password = password
+            self.email    = email
+            self.iddpt    = iddpt
+            self.idrole   = idrole
+class Dpt:
+     def __init__(self,iddpt,namedpt):
+            self.iddpt    = iddpt
+            self.namedpt  = namedpt
+class Rol:
+     def __init__(self,idrole,namerole):
+            self.idrole     = idrole
+            self.namerole   = namerole
+            
+    
