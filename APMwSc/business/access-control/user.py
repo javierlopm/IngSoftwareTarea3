2f81c11a1beb1754699b88cb68e21108a5a03e3a
Javier Lopez <javierlopm@hotmail.com>
2015-05-04 11:17:40 -0430

    Cambios en inicializacion de usuario

--- APMwSc/business/access-control/user.py
+++ APMwSc/business/access-control/user.py
@@ -1,11 +1,26 @@
-from sqlalchemy.sql.schema import PrimaryKeyConstraint
-class clsUser(db.Model):
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+
+class tbUser(db.Model):
     __tablename__ = 'user'
+    
     fullname = db.column(db.string(50),              unique=False)
     username = db.column(db.string(16),         primary_key=True )
     password = db.column(db.string(16),              unique=False)
     email    = db.column(db.string(30),              unique=True )
-    iddpt    = db.column(db.Integer   ,db.ForeignKey('dpt.iddpt'))
-    idrole   = db.column(db.Integer   ,db.ForeignKey('role.idrole'))
-
+    iddpt    = db.column(db.Integer   ,db.ForeignKey('tbdpt.iddpt'))
+    idrole   = db.column(db.Integer   ,db.ForeignKey('tbrole.idrole'))
     
+    def __init__(self,fullname,username,password,email,iddpt,idrole):
+        self.fullname() = fullname
+        self.username() = username
+        self.password() = password
+        self.email() = email
+        self.iddpt() = iddpt #departamento
+        self.idrole() = idrole #role
+        
+
+    def insert(self):
+        
