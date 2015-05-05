6854af9ae1da4aad1ff3ab555ca264f66559a514
Javier Lopez <javierlopm@hotmail.com>
2015-05-04 22:51:08 -0430

    Model controla los html y archivos estaticos. Version funcional de las vistas

--- APMwSc/business/access-control/user.py
+++ APMwSc/business/access-control/user.py
@@ -1,26 +1,30 @@
 from flask import Flask
+from model import db
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
 class tbUser(db.Model):
     __tablename__ = 'user'
-    
-    fullname = db.column(db.string(50),              unique=False)
-    username = db.column(db.string(16),         primary_key=True )
-    password = db.column(db.string(16),              unique=False)
-    email    = db.column(db.string(30),              unique=True )
+    fullname = db.column(db.String(50),              unique=False)
+    username = db.column(db.String(16),         primary_key=True )
+    password = db.column(db.String(16),              unique=False)
+    email    = db.column(db.String(30),              unique=True )
     iddpt    = db.column(db.Integer   ,db.ForeignKey('tbdpt.iddpt'))
     idrole   = db.column(db.Integer   ,db.ForeignKey('tbrole.idrole'))
     
-    def __init__(self,fullname,username,password,email,iddpt,idrole):
+    
+    def __init__(self,fullname,username,password,email,iddpt,idrole):        
         self.fullname() = fullname
         self.username() = username
         self.password() = password
         self.email() = email
         self.iddpt() = iddpt #departamento
         self.idrole() = idrole #role
-        
+    
 
-    def insert(self):
         
--- APMwSc/data/model.py
+++ APMwSc/data/model.py
@@ -1,3 +1,47 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import psycopg2
+from flask.templating import render_template
+import os
+import jinja2
+from jinja2.loaders import FileSystemLoader
+
+dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '../..'))
+app = Flask(__name__,template_folder=dir+"/",static_folder=dir+"/static")
+
+print(dir+"/static")
+
+app.secret_key = "justALamePass"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dsdad:team@localhost/APMwSc'
+
+
+print("realpath "+os.path.dirname(os.path.realpath(__file__)))
+
+print(dir+"/")
+
+@app.route("/")
+def index():
+    return render_template("/index.html")
+
+@app.route("/dpt")
+def dpt():
+    return render_template("/presentation/access-control/dpt.html")
+
+@app.route("/role")
+def role():
+    return render_template("/presentation/access-control/role.html")
+
+@app.route("/login")
+def login():
+    return render_template("/presentation/access-control/login.html")
+
+@app.route("/user")
+def user():
+    return render_template("/presentation/access-control/user.html")
+
+    
+db = SQLAlchemy(app)
+
 class User:
      def __init__(self,fullname,username,password,email,iddpt,idrole):
             self.fullname = fullname
@@ -15,4 +59,5 @@ class Rol:
             self.idrole     = idrole
             self.namerole   = namerole
             
-    
+if __name__ == "__main__":
+    app.run(debug=True)
